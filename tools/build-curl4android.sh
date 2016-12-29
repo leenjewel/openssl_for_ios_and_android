9d114db72e171973a2e7f458a8596f2d439d6262
liyin <liy@soulgame.com>
2016-12-29 14:14:32 +0800

    build output with make install

--- tools/build-curl4android.sh
+++ tools/build-curl4android.sh
@@ -28,9 +28,8 @@ pwd_path="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 ARCHS=("android" "android-armv7" "android64-arm64" "android-x86" "android-x86_64" "mips" "mips64")
 OUTNAME=("armeabi" "armeabi-v7a" "arm64-v8a" "x86" "x86_64" "mips" "mips64")
 LIB_NAME="curl-7.51.0"
-LIB_DEST_DIR="libs"
 HEADER_DEST_DIR="include"
-rm -rf "${HEADER_DEST_DIR}" "${LIB_DEST_DIR}" "${LIB_NAME}"
+rm -rf "${HEADER_DEST_DIR}" "${LIB_NAME}"
 NDK=$NDK_ROOT
 ANDROID_PLATFORM="android-23"
 # Unarchive library, then configure and make for specified architectures
@@ -41,36 +40,50 @@ configure_make()
    pushd .; cd "${LIB_NAME}";
 
    if [ "$ARCH" == "android" ]; then
+       PREFIX_DIR="${pwd_path}/../output/android/curl-android-armeabi"
+       OPENSSL_DIR="${pwd_path}/../output/android/openssl-android-armeabi"
        export ARCH_FLAGS="-mthumb"
        export ARCH_LINK=""
        export TOOL="arm-linux-androideabi"
        NDK_FLAGS="--platform=$ANDROID_PLATFORM --toolchain=arm-linux-androideabi-4.9 --install-dir=`pwd`/android-toolchain"
    elif [ "$ARCH" == "android-armv7" ]; then
+       PREFIX_DIR="${pwd_path}/../output/android/curl-android-armeabi-v7a"
+       OPENSSL_DIR="${pwd_path}/../output/android/openssl-android-armeabi-v7a"
        export ARCH_FLAGS="-march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16"
        export ARCH_LINK="-march=armv7-a -Wl,--fix-cortex-a8"
        export TOOL="arm-linux-androideabi"
        NDK_FLAGS="--platform=$ANDROID_PLATFORM --toolchain=arm-linux-androideabi-4.9 --install-dir=`pwd`/android-toolchain"
    elif [ "$ARCH" == "android64-arm64" ]; then
+       PREFIX_DIR="${pwd_path}/../output/android/curl-android-arm64-v8a"
+       OPENSSL_DIR="${pwd_path}/../output/android/openssl-android-arm64-v8a"
        export ARCH_FLAGS=""
        export ARCH_LINK=""
        export TOOL="aarch64-linux-android"
        NDK_FLAGS="--platform=$ANDROID_PLATFORM --toolchain=aarch64-linux-android-4.9 --install-dir=`pwd`/android-toolchain"
    elif [ "$ARCH" == "android-x86" ]; then
+       PREFIX_DIR="${pwd_path}/../output/android/curl-android-x86"
+       OPENSSL_DIR="${pwd_path}/../output/android/openssl-android-x86"
        export ARCH_FLAGS="-march=i686 -msse3 -mstackrealign -mfpmath=sse"
        export ARCH_LINK=""
        export TOOL="i686-linux-android"
        NDK_FLAGS="--platform=$ANDROID_PLATFORM --toolchain=x86-4.9 --install-dir=`pwd`/android-toolchain"
    elif [ "$ARCH" == "android-x86_64" ]; then
+       PREFIX_DIR="${pwd_path}/../output/android/curl-android-x86_64"
+       OPENSSL_DIR="${pwd_path}/../output/android/openssl-android-x86_64"
        export ARCH_FLAGS=""
        export ARCH_LINK=""
        export TOOL="x86_64-linux-android"
        NDK_FLAGS="--platform=$ANDROID_PLATFORM --toolchain=x86_64-4.9 --install-dir=`pwd`/android-toolchain"
    elif [ "$ARCH" == "mips" ]; then
+       PREFIX_DIR="${pwd_path}/../output/android/curl-android-mips"
+       OPENSSL_DIR="${pwd_path}/../output/android/openssl-android-mips"
        export ARCH_FLAGS=""
        export ARCH_LINK=""
        export TOOL="mipsel-linux-android"
        NDK_FLAGS="--platform=$ANDROID_PLATFORM --toolchain=mipsel-linux-android-4.9 --install-dir=`pwd`/android-toolchain"
    elif [ "$ARCH" == "mips64" ]; then
+       PREFIX_DIR="${pwd_path}/../output/android/curl-android-mips64"
+       OPENSSL_DIR="${pwd_path}/../output/android/openssl-android-mips64"
        export ARCH_FLAGS=""
        export ARCH_LINK=""
        export TOOL="mips64el-linux-android"
@@ -92,11 +105,10 @@ configure_make()
    export CXXFLAGS="${ARCH_FLAGS} -fpic -ffunction-sections -funwind-tables -fstack-protector -fno-strict-aliasing -finline-limit=64 -frtti -fexceptions "
    export CFLAGS="${ARCH_FLAGS} -fpic -ffunction-sections -funwind-tables -fstack-protector -fno-strict-aliasing -finline-limit=64 "
    export LDFLAGS="${ARCH_LINK}"
-   mkdir -p ../$LIB_DEST_DIR/$OUT
-   cp ${pwd_path}/openssl-${OUT}/libssl.a ${SYSROOT}/usr/lib/
-   cp ${pwd_path}/openssl-${OUT}/libcrypto.a ${SYSROOT}/usr/lib/
-   cp -r ${pwd_path}/openssl-${OUT}/include/openssl ${SYSROOT}/usr/include/
-   ./configure --prefix=${pwd_path}/$LIB_DEST_DIR/$OUT \
+   cp ${OPENSSL_DIR}/lib/libssl.a ${SYSROOT}/usr/lib/
+   cp ${OPENSSL_DIR}/lib/libcrypto.a ${SYSROOT}/usr/lib/
+   cp -r ${OPENSSL_DIR}/include/openssl ${SYSROOT}/usr/include/
+   ./configure --prefix=${PREFIX_DIR} \
        --with-sysroot=${SYSROOT} \
        --host=${TOOL} \
        --with-ssl=/usr \
