d30843942ef089efb4845b3694071d98c0e31b86
李樟取 <lizhangqu@weidian.com>
2017-07-18 12:25:23 +0800

    * update protobuf to 3.3.0

--- tools/build-protobuf4android.sh
+++ tools/build-protobuf4android.sh
@@ -20,7 +20,7 @@ source ./_shared.sh
 
 # Setup architectures, library name and other vars + cleanup from previous runs
 LIB_NAME="protobuf"
-LIB_VERSION="3.1.0"
+LIB_VERSION="3.3.0"
 LIB_FILENAME=${LIB_NAME}-${LIB_VERSION}
 LIB_DEST_DIR=${TOOLS_ROOT}/libs
 # rm -rf ${LIB_DEST_DIR}
